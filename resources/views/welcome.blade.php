ccaff9f0970cb2aada8d300853e22d9c405f7156
Luis Parrado <luisprmat@gmail.com>
2023-02-16 17:30:10 -0500

    Create translation keys in Spanish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,7 +137,7 @@
     <div class="container feature pt-5">
         <div class="d-flex flex-column text-center mb-5">
             <h4 class="text-primary font-weight-bold">Why Choose Us?</h4>
-            <h4 class="display-4 font-weight-bold">Benifits of Joining Our GYM</h4>
+            <h4 class="display-4 font-weight-bold">Benefits of Joining Our GYM</h4>
         </div>
         <div class="row">
             <div class="col-md-6 mb-5">
@@ -600,7 +600,7 @@
                 <div class="col-md-6">
                     <div class="pr-md-3 d-none d-md-block">
                         <h4 class="text-primary">Body Mass Index </h4>
-                        <h4 class="display-4 text-white font-weight-bold mb-4">Whate is BMI?</h4>
+                        <h4 class="display-4 text-white font-weight-bold mb-4">What is BMI?</h4>
                         <p class="m-0 text-white">Vero elitr lorem magna justo magna justo at justo est ipsum sed clita
                             lorem dolor ipsum sed. Lorem sea lorem vero. Sanct dolor clita clita rebum kasd magna erat
                             diam</p>
@@ -808,65 +808,5 @@
         </div>
     </div>
     <!-- Testimonial End -->
-
-
-    <!-- Blog Start -->
-    <div class="container pt-5">
-        <div class="d-flex flex-column text-center mb-5">
-            <h4 class="text-primary font-weight-bold">Our Blog</h4>
-            <h4 class="display-4 font-weight-bold">Latest Article From Blog</h4>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 mb-5 blog-item">
-                <img class="img-fluid mb-4" src="img/blog-1.jpg" alt="Image">
-                <div class="d-flex align-items-center mb-4">
-                    <div class="d-flex flex-column align-items-center justify-content-center rounded-circle bg-primary text-white"
-                        style="width: 80px; height: 80px;">
-                        <small>01</small>
-                        <strong class="text-uppercase m-0 text-white">Jan</strong>
-                        <small>2045</small>
-                    </div>
-                    <div class="pl-3">
-                        <h3 class="font-weight-bold">Lorem ipsum dolor sit amet</h3>
-                        <div class="d-flex">
-                            <small class="mr-2 text-muted"><i class="fa fa-user"></i> Admin</small>
-                            <small class="mr-2 text-muted"><i class="fa fa-folder"></i> Web Design</small>
-                            <small class="mr-2 text-muted"><i class="fa fa-comments"></i> 15 Comments</small>
-                        </div>
-                    </div>
-                </div>
-                <p>Eos kasd et sea labore. Et sed amet magna elitr amet, stet et amet erat nonumy labore lorem ut eos,
-                    sed ut at tempor diam no. Kasd sit ea amet lorem et sea, ipsum sed dolor dolores sed dolore eirmod
-                    vero dolor sed kasd accusam</p>
-                <a class="btn btn-outline-primary mt-2 px-3" href="">Read More <i
-                        class="fa fa-angle-right"></i></a>
-            </div>
-            <div class="col-lg-6 mb-5 blog-item">
-                <img class="img-fluid mb-4" src="img/blog-2.jpg" alt="Image">
-                <div class="d-flex align-items-center mb-4">
-                    <div class="d-flex flex-column align-items-center justify-content-center rounded-circle bg-primary text-white"
-                        style="width: 80px; height: 80px;">
-                        <small>01</small>
-                        <strong class="text-uppercase m-0 text-white">Jan</strong>
-                        <small>2045</small>
-                    </div>
-                    <div class="pl-3">
-                        <h3 class="font-weight-bold">Lorem ipsum dolor sit amet</h3>
-                        <div class="d-flex">
-                            <small class="mr-2 text-muted"><i class="fa fa-user"></i> Admin</small>
-                            <small class="mr-2 text-muted"><i class="fa fa-folder"></i> Web Design</small>
-                            <small class="mr-2 text-muted"><i class="fa fa-comments"></i> 15 Comments</small>
-                        </div>
-                    </div>
-                </div>
-                <p>Eos kasd et sea labore. Et sed amet magna elitr amet, stet et amet erat nonumy labore lorem ut eos,
-                    sed ut at tempor diam no. Kasd sit ea amet lorem et sea, ipsum sed dolor dolores sed dolore eirmod
-                    vero dolor sed kasd accusam</p>
-                <a class="btn btn-outline-primary mt-2 px-3" href="">Read More <i
-                        class="fa fa-angle-right"></i></a>
-            </div>
-        </div>
-    </div>
-    <!-- Blog End -->
 
 </x-app-layout>
